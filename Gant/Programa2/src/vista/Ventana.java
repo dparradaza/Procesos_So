a4cf82f326e32055d6d4662149745aa45c76c418
dparradaza <diego16parra@hotmail.com>
2019-06-01 17:35:48 -0500

    Correcciones

--- Gant/Programa2/src/vista/Ventana.java
+++ Gant/Programa2/src/vista/Ventana.java
@@ -28,7 +28,7 @@ import javax.swing.border.Border;
 public class Ventana extends JFrame implements ActionListener {
 
     //private JTextArea caja1;
-    private JTable caja1, caja2;
+    private JTable procesos, gant;
     private JLabel texto, texto1, autor, pal, pal2, nopal, contarL, contarP, eliminar;
     private JButton cerrar, operar;
     private JComboBox combo;
@@ -62,6 +62,7 @@ public class Ventana extends JFrame implements ActionListener {
         combo.addItem("✔ Crear Procesos");
         combo.addItem("✔ Iniciar Simulación");
         combo.addItem("✔ Limpiar");
+
         //combo.addItem("✔ Ver reporte");
         //combo.addItem("✔ Pensum");
         combo.setBounds(getWidth() / 2 + 100, 80, 170, 22);
@@ -76,7 +77,7 @@ public class Ventana extends JFrame implements ActionListener {
     public void PanelIzq() {
         // Creacion del JPanel Izquierda
         panelIzquierdo = new JPanel();
-        panelIzquierdo.setBounds(0, 0, (getWidth() / 2) -150, getHeight());
+        panelIzquierdo.setBounds(0, 0, (getWidth() / 2) - 150, getHeight());
         panelIzquierdo.setBackground(new Color(187, 187, 187));
         panelIzquierdo.setLayout(null);
         this.add(panelIzquierdo);
@@ -86,7 +87,7 @@ public class Ventana extends JFrame implements ActionListener {
     public void PanelDer() {
         // Creacion del JPanel Derecho
         panelDerecho = new JPanel();
-        panelDerecho.setBounds((getWidth() / 2)+150, 0, getWidth(), getHeight());
+        panelDerecho.setBounds((getWidth() / 2) + 150, 0, getWidth(), getHeight());
         panelDerecho.setBackground(new Color(250, 162, 96));
         panelDerecho.setLayout(null);
         this.add(panelDerecho);
@@ -111,9 +112,9 @@ public class Ventana extends JFrame implements ActionListener {
         texto1.setBounds(getWidth() / 2 + 120, 40, 390, 20);
         panelDerecho.add(texto1);
         texto1.updateUI();
-        
+
         //tiempo 
-        tiempo_real= new JLabel();
+        tiempo_real = new JLabel();
         tiempo_real.setText("0");
         panelIzquierdo.add(tiempo_real);
         tiempo_real.setBounds(getWidth() / 25 - 25, getHeight() / 2 + 20, 500, 30);
@@ -141,17 +142,20 @@ public class Ventana extends JFrame implements ActionListener {
         //Cajas de texto
         JScrollPane scroll = new JScrollPane();
 
-        caja1 = new JTable();
+        procesos = new JTable();
         // caja1.setBounds(getWidth() / 2 + 10, 200, 400, 120);
-        caja1.setBackground(new Color(32, 50, 72));
-        caja1.setForeground(new Color(235, 196, 56));
+        procesos.setBackground(new Color(32, 50, 72));
+        procesos.setForeground(new Color(235, 196, 56));
+        procesos.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         //caja1.setLineWrap(true);
-        scroll.setViewportView(caja1);
-        scroll.setBounds((getWidth() / 2) -150 + 10, 150, 660, 180);
-        panelDerecho.add(scroll);
-        caja1.updateUI();
+        scroll.setViewportView(procesos);
+        scroll.setBounds((getWidth() / 2) - 150 + 10, 150, 660, 180);
 
-        caja1.setModel(new javax.swing.table.DefaultTableModel(
+        panelDerecho.add(scroll);
+
+        procesos.updateUI();
+
+        procesos.setModel(new javax.swing.table.DefaultTableModel(
                 new Object[][]{
                     {null, null, null, null, null, null},
                     {null, null, null, null, null, null},
@@ -252,10 +256,10 @@ public class Ventana extends JFrame implements ActionListener {
                     {null, null, null, null, null, null},
                     {null, null, null, null, null, null},
                     {null, null, null, null, null, null},
-                    {null, null, null, null, null, null}
-                },
+                    {null, null, null, null, null, null}},
                 new String[]{
-                    "Proceso", "Tiempo de llegada", "Tiempo de ráfaga", "Tiempo de comienzo", "Tiempo de finalización", "Tiempo de espera"
+                    "Proceso", "Tiempo de llegada", "Tiempo de ráfaga",
+                    "Tiempo de comienzo", "Tiempo de finalización", "Tiempo de espera"
                 }
         ) {
             boolean[] canEdit = new boolean[]{
@@ -269,17 +273,18 @@ public class Ventana extends JFrame implements ActionListener {
 
         //Caja 2
         JScrollPane scroll2 = new JScrollPane();
-        caja2 = new JTable();
+        gant = new JTable();
+        gant.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         //caja2.setBounds(getWidth() / 2 + 10, 400, 400, 120);
         //caja.setBorder(bordes);
         //caja.setCursor(mano);
-        caja2.setBackground(new Color(32, 50, 72));
-        caja2.setForeground(new Color(235, 196, 56));
-        scroll2.setViewportView(caja2);
-        scroll2.setBounds((getWidth() / 2) -150 + 10, 350, 660, 230);
+        gant.setBackground(new Color(32, 50, 72));
+        gant.setForeground(new Color(235, 196, 56));
+        scroll2.setViewportView(gant);
+        scroll2.setBounds((getWidth() / 2) - 150 + 10, 350, 660, 230);
         panelDerecho.add(scroll2);
 
-        caja2.setModel(new javax.swing.table.DefaultTableModel(
+        gant.setModel(new javax.swing.table.DefaultTableModel(
                 new Object[][]{
                     {"1", null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null},
                     {"2", null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null},
@@ -314,18 +319,11 @@ public class Ventana extends JFrame implements ActionListener {
                 },
                 new String[]{
                     "P", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "TE", "TR"
-                }
-        ) {
-            boolean[] canEdit = new boolean[]{
-                true, true, true, true, true, true, true, true, false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true
-            };
+                }) {
 
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit[columnIndex];
-            }
         });
 
-        caja2.updateUI();
+        gant.updateUI();
 
         //------------------------
         cerrar = new JButton("X");
@@ -373,37 +371,35 @@ public class Ventana extends JFrame implements ActionListener {
     }
 
     public void graficar(String matriz[][]) {
-        caja2.setModel(new javax.swing.table.DefaultTableModel(
+        gant.setModel(new javax.swing.table.DefaultTableModel(
                 matriz,
                 new String[]{
                     "Iteración", "Proceso", "Servicios"
                 }
         ));
-        caja2.setRowHeight(30);
+        gant.setRowHeight(30);
 
     }
 
-    public JLabel getTiempo_real() {
-        return tiempo_real;
+   public JTable getjTableAgregarProcesos() {
+        return procesos;
     }
-
-    public JTable getjTable1() {
-        return caja1;
+   
+       public JTable getjTable1() {
+        return gant;
     }
 
     public void setjTable1(JTable jTable1) {
-        this.caja1 = jTable1;
+        this.gant = jTable1;
+    }
+    
+    public JLabel getTiempo_real() {
+        return tiempo_real;
     }
 
     public void setTiempo_real(JLabel tiempo_real) {
         this.tiempo_real = tiempo_real;
     }
-
-    public JTable getjTableAgregarProcesos() {
-        return caja2;
-    }
-    
-
     @Override
     public void actionPerformed(ActionEvent e) {
         char c = (char) 65;
@@ -416,11 +412,11 @@ public class Ventana extends JFrame implements ActionListener {
             if (opcion.equals("✔ Crear Procesos")) {
 
             } else if (opcion.equals("✔ Iniciar Simulación")) {
-                controlador.vaciarTabla(caja1);
+                controlador.vaciarTabla(procesos);
                 controlador.insertarEnCola();
                 if (controlador.getCola() != null) {
-                    caja2.selectAll();
-                    controlador.agregarProceso(caja1, caja2);
+                    gant.selectAll();
+                    controlador.agregarProceso(procesos, gant);
                     controlador.start();
                 }
 
