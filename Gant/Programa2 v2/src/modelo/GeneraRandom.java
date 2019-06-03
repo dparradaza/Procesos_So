512c4eacf767cfdcb8b3a02b95c5dd0182941932
Oscar Bautista <38815533+adba98@users.noreply.github.com>
2019-06-03 17:01:38 -0500

    Add files via upload

---
+++ Gant/Programa2 v2/src/modelo/GeneraRandom.java
@@ -0,0 +1,35 @@
+package modelo;
+
+import java.util.ArrayList;
+
+public class GeneraRandom {
+    
+    public GeneraRandom(){}
+    
+    
+    public ArrayList<Integer> generarComienzo (int noProc){//noProc numero de proceso
+        ArrayList<Integer> tiemposLlegada = new ArrayList<>();
+        for (int i = 0; i < noProc; i++) {
+            int r;
+            if (tiemposLlegada.isEmpty()) {
+                r = (int) Math.floor(Math.random() * 5);//Math.random()*ValorLimite+ValorInicial
+                tiemposLlegada.add(r);
+            } else {
+                r = ((int) Math.floor(Math.random() * 5));
+                r = r + tiemposLlegada.get(i - 1);
+                tiemposLlegada.add(r);  
+            }
+        }
+        return(tiemposLlegada);                       
+    }
+    
+    public ArrayList<Integer> generarRafaga(int noProc){
+        ArrayList<Integer> tiemposRafaga = new ArrayList<>();
+        int tRafaga;
+        for (int j = 0; j < noProc; j++) {
+            tRafaga = (int)Math.floor(Math.random()* 10 + 1);  
+            tiemposRafaga.add(tRafaga);
+        }
+        return tiemposRafaga;
+    }   
+}
